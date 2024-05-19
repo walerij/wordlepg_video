98e0ab298f0f53e31b3922eee63aca62f275edab
ZHdanov Valery <becwal@yandex.ru>
2024-05-19 19:59:52 +0300

    итерации 2-3

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
+from game import Game
+from symbol import Symbol
 
 pygame.init()
 
 W,H = 600,600
 
+sym_w = 50
+sym_h = 50
+
 sc = pygame.display.set_mode((W, H))
 pygame.display.set_caption("Wordle 2.0")
+
+curr_game = Game()
+sym = None
+    #Symbol(curr_game.quest, W/2, H/2)
 
 
 while True:
@@ -15,6 +24,12 @@ while True:
 
 
     sc.fill((0,0,0))
+    pos=0
+    for i in curr_game.quest:
+        sym = Symbol(i, W/3+pos, H/2)
+        sc.blit(sym.text, sym.rect)
+        pos += sym_w
+
     pygame.display.update()
 
 
---
+++ symbol.py
@@ -0,0 +1,11 @@
+import pygame
+class Symbol(pygame.sprite.Sprite):
+    def __init__(self, text, x, y):
+        self.font = pygame.font.SysFont("courier",72)
+        self.default_color = ((0,0,0),(201, 201, 197))
+        self.correct_color = ((0,0,0),(14, 163, 11))
+        self.wrong_color = ((0, 0, 0), (232, 28, 21))
+        self.textcolor = self.default_color[0]
+        self.bgcolor = self.default_color[1]
+        self.text = self.font.render(text, True, self.textcolor, self.bgcolor)
+        self.rect = self.text.get_rect(center=(x,y))
