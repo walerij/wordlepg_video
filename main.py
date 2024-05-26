1c6faec6f331934b141e0f98c73a23880302f9b6
ZHdanov Valery <becwal@yandex.ru>
2024-05-26 09:35:46 +0300

    проверка слова и вывод результата проверки по цветам

--- main.py
+++ main.py
@@ -30,19 +30,24 @@ while True:
     sc.fill((0,0,0))
     pos_w=0
     pos_h =0
-    '''
-    for i in curr_game.quest:
-        sym = Symbol(i, W/3+pos_w, H/2+pos_h)
-        sc.blit(sym.text, sym.rect)
-        pos += sym_w
-   '''
+
     for i in range(matrix.level):
         for j in range(5):
-            sym = Symbol(matrix.text_matrix[i][j][0], W / 3 + pos_w, H /5 + pos_h)
+            sym = Symbol(matrix.text_matrix[i][j][0], W / 3 + pos_w, H /5 + pos_h,matrix.text_matrix[i][j][1] )
             sc.blit(sym.text, sym.rect)
             pos_w += sym_w
         pos_h += sym_h
         pos_w =0
+
+    pressed = pygame.key.get_pressed()
+
+    if (pressed[pygame.K_SPACE]):
+        print(curr_game.quest)
+        word = 'кобра'
+        s = curr_game.check(word)
+        matrix.text_matrix[0] = s
+
+
 
 
 
--- symbol.py
+++ symbol.py
@@ -1,11 +1,17 @@
 import pygame
 class Symbol(pygame.sprite.Sprite):
-    def __init__(self, text, x, y):
+    def __init__(self, text, x, y, color_):
         self.font = pygame.font.SysFont("courier",40)
         self.default_color = ((0,0,0),(201, 201, 197))
         self.correct_color = ((0,0,0),(14, 163, 11))
         self.wrong_color = ((0, 0, 0), (232, 28, 21))
         self.textcolor = self.default_color[0]
         self.bgcolor = self.default_color[1]
+        if (color_=='red'):
+            self.textcolor = self.wrong_color[0]
+            self.bgcolor = self.wrong_color[1]
+        elif (color_=='green'):
+            self.textcolor = self.correct_color[0]
+            self.bgcolor = self.correct_color[1]
         self.text = self.font.render(text, True, self.textcolor, self.bgcolor)
         self.rect = self.text.get_rect(center=(x,y))
